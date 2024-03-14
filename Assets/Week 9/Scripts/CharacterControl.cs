f296361fa373a9b42a78c0e7c1997b4bf56eae0f
Bradash <brandonhutchison6@gmail.com>
2024-03-14 00:31:36 -0400

    Task 4b) Complete: display currently selected type

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,19 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public static string VillagerType;
+    public TextMeshProUGUI textMeshProUGUI;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
-        if(SelectedVillager != null)
+        if (SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
+            VillagerType = "None";
         }
         SelectedVillager = villager;
+        VillagerType = villager.name;
         SelectedVillager.Selected(true);
     }
-    
+    private void Update()
+    {
+        textMeshProUGUI.text = VillagerType;
+    }
+
 }
