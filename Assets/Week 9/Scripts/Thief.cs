e4f29bb5f920f0232e1f6145a901c3c4da0a098e
Bradash <hutbrand@shernet.sheridancollege.ca>
2024-03-20 15:05:54 -0400

    1c complete: knives spawn after a delay.

--- Assets/Week 9/Scripts/Thief.cs
+++ Assets/Week 9/Scripts/Thief.cs
@@ -9,33 +9,37 @@ public class Thief : Villager
     public Transform spawnPoint1;
     public Transform spawnPoint2;
     float maxTime = 0.5f;
+    Coroutine dashing;
     public override ChestType CanOpen()
     {
         return ChestType.Theif;
-}
+    }
     protected override void Attack()
     {
-        StartCoroutine(Dash());
+        if (dashing != null) { 
+        StopCoroutine(dashing);
+        }
+
+        StopCoroutine(Dash());
         destination = transform.position;
-
-
         destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        StartCoroutine(Dash());
     }
     IEnumerator Dash()
     {
         speed = 8;
-        yield return new WaitForSeconds(maxTime);
-        base.Attack();
-        Instantiate(dagger, spawnPoint1.position, spawnPoint1.rotation);
-        Instantiate(dagger, spawnPoint2.position, spawnPoint2.rotation);
-        speed = 3;
-    }
-    protected override void Update()
-    {
         if (Input.GetMouseButtonDown(0))
         {
-           speed = 3;
+            speed = 3;
+            yield break;
         }
-        base.Update();
+
+        yield return new WaitForSeconds(maxTime);
+        base.Attack();
+        yield return new WaitForSeconds(0.2f);
+        Instantiate(dagger, spawnPoint1.position, spawnPoint1.rotation);
+        yield return new WaitForSeconds(0.2f);
+        Instantiate(dagger, spawnPoint2.position, spawnPoint2.rotation);
+        speed = 3;
     }
 }
